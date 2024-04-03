569f528e88f5b254b9001b04407bc9f4461fe793
Gabriel Zanardi Nogueira <159185296+Gabriel-Zanardi@users.noreply.github.com>
2024-04-03 18:27:21 +0000

    aded favicon, css, pyramid png and index base code

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Gabriel
+// Created on: April 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function myButtonClicked() {
+  //input
+  const firstBase = document.getElementById("base-a").value
+  const secondBase = document.getElementById("base-b").value
+  const heightOfTrapezium = document.getElementById("height").value
+  //process
+  const areaHalfway = (firstBase * secondBase) * heightOfTrapezium
+  const areaAnswer = areaHalfway / 2
+
+  //output
+  document.getElementById("answer").innerHTML =
+    "The area of this trapezium is: " + areaAnswer + " mm²."
+}
